3cfe459ead8bb90135efc33c610c405f86cdc367
Fan <Fan@qq.com>
2022-04-07 21:37:12 +0800

    first commit

---
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+/**
+ * @author wzf
+ * @create 2022-04-07 21:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
